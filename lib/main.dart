9257c6b8ae9d7e62211921f3ea05c0ff0f88248e
Michal Salasek <xsalasek@fi.muni.cz>
2022-04-03 20:57:52 +0200

    feat: add firebase support

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:ride_with_me/pages/initial_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
