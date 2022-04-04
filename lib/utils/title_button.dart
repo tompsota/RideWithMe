522647beea479be90c346acec70235969763eb4e
tompsota <to.psota@gmail.com>
2022-04-04 11:42:43 +0200

    feat: Added text field for title in ride view

--- lib/utils/title_button.dart
+++ lib/utils/title_button.dart
@@ -14,31 +14,16 @@ class _TitleButtonState extends State<TitleButton> {
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.symmetric(vertical: 8),
-        child: TextButton(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              TextField(
-                controller: _textController,
-                onChanged: (text) {
-                  _textController.text = text;
-                },
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              Icon(
-                Icons.content_copy,
-                color: Colors.grey,
-                // size: 30.0,
-              ),
-            ],
+        child: TextFormField(
+          controller: _textController,
+          onFieldSubmitted: (text) {
+            _textController.text = text;
+          },
+          decoration: const InputDecoration(
+            border: InputBorder.none,
+            suffixIcon: Icon(Icons.edit),
           ),
-          style: TextButton.styleFrom(
-            primary: Theme.of(context).primaryColor,
-            // backgroundColor: Colors.white,
-            padding: EdgeInsets.all(20),
-            // textStyle:
-          ),
-          onPressed: () {},
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Theme.of(context).primaryColor),
         ));
   }
 }
