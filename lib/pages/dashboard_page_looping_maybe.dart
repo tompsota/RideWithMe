04c29b21fe07cfa276bd4026b6741a3c5d8bd9ac
Michal Salasek <xsalasek@fi.muni.cz>
2022-05-22 22:02:14 +0200

    fix: potentially fixed Consumer-FutureBuilder looping in DashboardPage which repleted firestore quota

--- lib/pages/dashboard_page_looping_maybe.dart
+++ lib/pages/dashboard_page_looping_maybe.dart
@@ -14,112 +14,114 @@ class DashboardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 50),
-                child: SubmitButton(
-                  value: "REFRESH RIDES",
-                  callback: () async => Provider.of<RideFilterController>(context, listen: false).refreshRides(),
-                ),
-              ),
-            ),
-          ],
-        ),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 50),
-                child: SubmitButton(
-                  value: "FILTER RIDES",
-                  callback: () async {
-                    Navigator.push(
-                      context,
-                      // MaterialPageRoute(builder: (context) => FilterRidesPage()),
-                      MaterialPageRoute(builder: (context) => ChangeNotifierProvider.value(value: Provider.of<RideFilterController>(context, listen: false), child: FilterRidesPage())),
-                    );
-                  },
-                ),
-              ),
-            ),
-          ],
-        ),
-        // FutureBuilder<List<RideModel>>(
-        FutureBuilder<void>(
-          // future: getAllRides(),
-          future: Provider.of<RideFilterController>(context, listen: false).refreshRides(),
-          initialData: [],
-          // builder: (BuildContext context, AsyncSnapshot<List<RideModel>> snapshot) {
-          builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-            return Expanded(
-              // child: SingleChildScrollView(
+    return Consumer<RideFilterController>(builder: (context, filterController, child) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Expanded(
                 child: Padding(
                   padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 50),
-                  child: ListView(
-                    shrinkWrap: true,
-                    children: Provider.of<RideFilterController>(context).visibleRides.map((ride) => FutureBuilder<RideModel?>(
-                      // for filters we don't need participants or author (for number of participants we have ride.participantsIds)
-                      // we 'include' author for display ; getFullRide(ride) would fetch both author and participants
-                      future: getRideWithAuthor(ride),
-                      initialData: ride,
-                      builder: (BuildContext context, AsyncSnapshot<RideModel?> snapshot) {
-                        // TODO: add snapshot.hasData (etc.) checks
-                        final ride = snapshot.data!;
-                        return ListTile(
-                          title: Text('${ride.title}  ${(ride.isCompleted) ? "(COMPLETED)" : ""}'),
-                          subtitle: Text(
-                            "author: ${ride.author?.getFullName() ?? "Unknown"}, participants: ${ride.participantsIds.length}"),
-                          onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                            builder: (_) => ChangeNotifierProvider.value(
-                              value: Provider.of<UserStateController>(context),
-                              child: RideViewPage(rideBeingEdited: ride),
-                            )
-                          )));
-                      }))
-                    .toList()
+                  child: SubmitButton(
+                    value: "REFRESH RIDES",
+                    callback: () async => filterController.refreshRides(),
                   ),
                 ),
-              // ),
-            );
-          },
-        ),
-
-
-
-        //todo maybe use this for 'add ride' button
-        // FloatingActionButton(
-        //   onPressed: () {},
-        //   tooltip: 'Increment',
-        //   child: const Icon(Icons.add),
-        // ),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Consumer<UserStateController>(builder: (context, userController, child) {
-              return Expanded(
-                  child: Padding(
+              ),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Expanded(
+                child: Padding(
                   padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 50),
                   child: SubmitButton(
-                    value: "ADD RIDE",
-                    callback: () => Navigator.of(context).push(MaterialPageRoute(
-                      // builder: (_) => RideViewPage()
-                      builder: (context) => ChangeNotifierProvider.value(value: userController, child: RideViewPage()))
-                    ),
+                    value: "FILTER RIDES",
+                    callback: () async {
+                      Navigator.push(
+                        context,
+                        // MaterialPageRoute(builder: (context) => FilterRidesPage()),
+                        MaterialPageRoute(builder: (context) => ChangeNotifierProvider.value(value: filterController, child: FilterRidesPage())),
+                      );
+                    },
                   ),
                 ),
+              ),
+            ],
+          ),
+          // FutureBuilder<List<RideModel>>(
+          FutureBuilder<void>(
+            // future: getAllRides(),
+            future: filterController.refreshRides(),
+            initialData: [],
+            // builder: (BuildContext context, AsyncSnapshot<List<RideModel>> snapshot) {
+            builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+              return Expanded(
+                // child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 50),
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: filterController.visibleRides.map((ride) => FutureBuilder<RideModel?>(
+                        // for filters we don't need participants or author (for number of participants we have ride.participantsIds)
+                        // we 'include' author for display ; getFullRide(ride) would fetch both author and participants
+                        future: getRideWithAuthor(ride),
+                        initialData: ride,
+                        builder: (BuildContext context, AsyncSnapshot<RideModel?> snapshot) {
+                          // TODO: add snapshot.hasData (etc.) checks
+                          final ride = snapshot.data!;
+                          return ListTile(
+                            title: Text('${ride.title}  ${(ride.isCompleted) ? "(COMPLETED)" : ""}'),
+                            subtitle: Text(
+                              "author: ${ride.author?.getFullName() ?? "Unknown"}, participants: ${ride.participantsIds.length}"),
+                            onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                              builder: (_) => ChangeNotifierProvider.value(
+                                value: Provider.of<UserStateController>(context),
+                                child: RideViewPage(rideBeingEdited: ride),
+                              )
+                            )));
+                        }))
+                      .toList()
+                    ),
+                  ),
+                // ),
               );
-            }),
-          ],
-        ),
-      ],
-    );
+            },
+          ),
+
+
+
+          //todo maybe use this for 'add ride' button
+          // FloatingActionButton(
+          //   onPressed: () {},
+          //   tooltip: 'Increment',
+          //   child: const Icon(Icons.add),
+          // ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Consumer<UserStateController>(builder: (context, userController, child) {
+                return Expanded(
+                    child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 50),
+                    child: SubmitButton(
+                      value: "ADD RIDE",
+                      callback: () => Navigator.of(context).push(MaterialPageRoute(
+                        // builder: (_) => RideViewPage()
+                        builder: (context) => ChangeNotifierProvider.value(value: userController, child: RideViewPage()))
+                      ),
+                    ),
+                  ),
+                );
+              }),
+            ],
+          ),
+        ],
+      );
+    });
   }
 }
 
